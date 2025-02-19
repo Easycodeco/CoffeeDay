12aed47ef6b6045ac9e6d1f818c037d9a179631b
Mubarak <co.easycode@gmail.com>
2025-02-19 13:28:45 +0300

    Initial Commit

---
+++ CoffeeDay/CoffeeDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeDayApp.swift
+//  CoffeeDay
+//
+//  Created by M3 on 19/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
